29d81b61144b767091fbadbb204d0a334b486440
Vyacheslav Gizov <vogizov@gmail.com>
2024-08-09 20:04:10 +0500

    For second review

--- exceptions.py
+++ exceptions.py
@@ -2,13 +2,5 @@ class UnsuccessfulResponseError(Exception):
     pass
 
 
-class FailedSendingMesageError(Exception):
-    pass
-
-
-class HttpRequestError(Exception):
-    pass
-
-
 class ServerError(Exception):
     pass
--- homework.py
+++ homework.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 from telebot import TeleBot
 import requests
 
-import exceptions
+from exceptions import UnsuccessfulResponseError, ServerError
 
 
 load_dotenv()
@@ -17,6 +17,9 @@ load_dotenv()
 PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+
+REQUIRED_CONSTANTS_NAMES = ['PRACTICUM_TOKEN', 'TELEGRAM_TOKEN',
+                            'TELEGRAM_CHAT_ID']
 
 RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
@@ -32,31 +35,31 @@ LOG_FORMAT = ('%(asctime)s - %(name)s - [%(levelname)s] - '
               '%(filename)s: %(funcName)s: %(lineno)s - %(message)s')
 
 CHECK_TOKENS_ERROR = 'Не обнаружены переменные окружения: {not_found_vars}'
-FAILED_SENDING = 'Сбой при отправке сообщения: "{message}".'
+FAILED_SENDING = ('При отправке сообщения "{message}" возникла следующая '
+                  'ошибка: {error}.')
 SUCCESSFUL_SENDING = 'Успешная отправка сообщения: "{message}".'
 REQUEST_ERROR = (
     'Сбой сети! При выполнении GET-запроса с использованием '
-    'requests.get() со следующими параметрами: {req_params} '
+    'requests.get() со следующими параметрами: {request_params} '
     'возникла ошибка: {error}.'
 )
 SERVER_ERROR = (
-    'Отказ сервера! При выполнении GET-запроса с использованием '
-    'requests.get() со следующими параметрами: {req_params} '
-    'получен следующий ответ: {response}.'
+    'Отказ сервера при выполнении GET-запроса с использованием '
+    'requests.get() со следующими параметрами: {request_params}. '
+    'Данные об ошибке: {error_info}.'
 )
 UNSUCCESSFUL_RESPONSE = (
     'Неожиданный статус ответа! При выполнении GET-запроса с использованием '
-    'requests.get() со следующими параметрами: {req_params} '
-    'получен ответ с кодом возврата: {resp_status}. Ожидаемый код: 200.'
+    'requests.get() со следующими параметрами: {request_params} '
+    'получен ответ с кодом возврата: {response_status}. Ожидаемый код: 200.'
 )
 WRONG_INPUT_DATA = 'Неожиданный тип данных ответа сервера: {data_type}.'
-HOMEWORKS_NOT_FOUND = 'Ожидаемый ключ {expected_key} отсутствует в ответе API.'
+KEY_NOT_FOUND_IN_RESPONSE = ('Ожидаемый ключ {key} отсутствует в'
+                             ' ответе API.')
 WRONG_DATA_TYPE = ('Неожиданный тип данных {data_type}, '
                    'полученных по ключу {key}.')
-HOMEWORK_NAME_NOT_FOUND = ('Ожидаемый ключ "homework_name" отсутствует '
-                           'в данных о домашней работе.')
-STATUS_NOT_FOUND = ('Ожидаемый ключ "status" отсутствует '
-                    'в данных о домашней работе.')
+KEY_NOT_FOUND_IN_HOMEWORK = ('Ожидаемый ключ {key} отсутствует '
+                             'в данных о домашней работе.')
 WRONG_HOMEWORK_STATUS = ('Неожиданный статус домашней работы "{status}", '
                          'обнаруженный в ответе API.')
 HOMEWORK_STATUS_IS_CHANGED = ('Изменился статус проверки работы '
@@ -70,15 +73,8 @@ logger = logging.getLogger(__name__)
 
 def check_tokens():
     """Проверит наличие требуемых переменных окружения."""
-    required_variables = {
-        'PRACTICUM_TOKEN': PRACTICUM_TOKEN,
-        'TELEGRAM_TOKEN': TELEGRAM_TOKEN,
-        'TELEGRAM_CHAT_ID': TELEGRAM_CHAT_ID,
-    }
-    not_found_vars = []
-    for var_name in required_variables.keys():
-        if required_variables[var_name] is None:
-            not_found_vars.append(var_name)
+    not_found_vars = [name for name in REQUIRED_CONSTANTS_NAMES
+                      if globals().get(name) is None]
     if not_found_vars:
         message = CHECK_TOKENS_ERROR.format(not_found_vars=not_found_vars)
         logger.critical(message)
@@ -86,44 +82,45 @@ def check_tokens():
 
 
 def send_message(bot, message):
-    """Отправит сообщение "message" в Телеграм."""
+    """Отправит сообщение "message" в Телеграм. В случае успеха вернет True."""
     try:
         bot.send_message(TELEGRAM_CHAT_ID, message)
         logger.debug(SUCCESSFUL_SENDING.format(message=message))
-    except Exception:
-        logger.exception(FAILED_SENDING.format(message=message))
+        return True
+    except Exception as error:
+        logger.exception(FAILED_SENDING.format(message=message, error=error))
 
 
 def get_api_answer(timestamp):
     """Сделает запрос к API Практикум.Домашка.
     В случе успеха вернет ответ API в виде словаря.
     """
-    req_params = {
+    request_params = {
         'url': ENDPOINT,
         'headers': HEADERS,
         'params': {'from_date': timestamp},
     }
     try:
-        response = requests.get(**req_params)
-        resp_json = response.json()
-        resp_status = response.status_code
+        response = requests.get(**request_params)
     except requests.RequestException as error:
-        raise exceptions.HttpRequestError(
-            REQUEST_ERROR.format(req_params=req_params, error=error)
+        raise ConnectionError(
+            REQUEST_ERROR.format(request_params=request_params, error=error)
         )
-    for item in ['code', 'error']:
-        if item in resp_json:
-            raise exceptions.ServerError(
-                SERVER_ERROR.format(req_params=req_params, response=resp_json)
-            )
-    if resp_status != HTTPStatus.OK:
-        raise exceptions.UnsuccessfulResponseError(
-            UNSUCCESSFUL_RESPONSE.format(
-                req_params=req_params,
-                resp_status=resp_status
-            )
-        )
-    return resp_json
+    response_json = response.json()
+    error_info = {key: response_json[key] for key in ['code', 'error']
+                  if key in response_json}
+    if error_info:
+        raise ServerError(SERVER_ERROR.format(
+            request_params=request_params,
+            error_info=error_info
+        ))
+    response_status = response.status_code
+    if response_status != HTTPStatus.OK:
+        raise UnsuccessfulResponseError(UNSUCCESSFUL_RESPONSE.format(
+            request_params=request_params,
+            response_status=response_status
+        ))
+    return response_json
 
 
 def check_response(response):
@@ -131,18 +128,18 @@ def check_response(response):
     В случае соответствие ожидаемому формату вернет
     полученный ответ в виде словаря.
     """
-    expected_key = 'homeworks'
     if not isinstance(response, dict):
         raise TypeError(WRONG_INPUT_DATA.format(data_type=type(response)))
-    if expected_key not in response:
-        raise KeyError(HOMEWORKS_NOT_FOUND.format(expected_key=expected_key))
-    if not isinstance(response[expected_key], list):
-        raise TypeError(
-            WRONG_DATA_TYPE.format(
-                data_type=type(response[expected_key]),
-                key=expected_key
-            )
+    if 'homeworks' not in response:
+        raise KeyError(
+            KEY_NOT_FOUND_IN_RESPONSE.format(key='homeworks')
         )
+    homeworks = response['homeworks']
+    if not isinstance(homeworks, list):
+        raise TypeError(WRONG_DATA_TYPE.format(
+                data_type=type(homeworks),
+                key='homeworks'
+            ))
     return response
 
 
@@ -152,22 +149,48 @@ def parse_status(homework):
     вернет строку с иформацией о статусе данной работы.
     """
     if 'homework_name' not in homework:
-        raise KeyError(HOMEWORK_NAME_NOT_FOUND)
+        raise KeyError(KEY_NOT_FOUND_IN_HOMEWORK.format(key='homework_name'))
     if 'status' not in homework:
-        raise KeyError(STATUS_NOT_FOUND)
+        raise KeyError(KEY_NOT_FOUND_IN_HOMEWORK.format(key='status'))
     status = homework['status']
     if status not in HOMEWORK_VERDICTS:
         raise ValueError(WRONG_HOMEWORK_STATUS.format(status=status))
-    return (
-        HOMEWORK_STATUS_IS_CHANGED.format(
-            homework_name=homework['homework_name'],
-            verdict=HOMEWORK_VERDICTS[status]
+    return HOMEWORK_STATUS_IS_CHANGED.format(
+        homework_name=homework['homework_name'],
+        verdict=HOMEWORK_VERDICTS[status]
         )
-    )
 
 
 def main():
     """Основной цикл работы бота."""
+    check_tokens()
+    bot = TeleBot(token=TELEGRAM_TOKEN)
+    timestamp = int(time.time())
+    last_message = None
+
+    while True:
+        try:
+            response_data = check_response(get_api_answer(timestamp))
+            homeworks = response_data['homeworks']
+            if not homeworks:
+                logger.debug(HOMEWORK_STATUS_NOT_CHANGED)
+                continue
+            message = parse_status(homeworks[0])
+            if last_message != message:
+                if send_message(bot, message):
+                    timestamp = response_data.get('current_date', timestamp)
+                    last_message = message
+        except Exception as error:
+            message = ERROR_MESSAGE.format(error=error)
+            logger.error(message, exc_info=True)
+            if last_message != message:
+                if send_message(bot, message):
+                    last_message = message
+        finally:
+            time.sleep(RETRY_PERIOD)
+
+
+if __name__ == '__main__':
     logging.basicConfig(
         format=LOG_FORMAT,
         level=logging.DEBUG,
@@ -180,30 +203,4 @@ def main():
             ),
         ],
     )
-
-    check_tokens()
-    bot = TeleBot(token=TELEGRAM_TOKEN)
-    timestamp = int(time.time())
-    last_message = None
-
-    while True:
-        try:
-            response_data = check_response(get_api_answer(timestamp))
-            if len(response_data['homeworks']) == 0:
-                logger.debug(HOMEWORK_STATUS_NOT_CHANGED)
-                continue
-            message = parse_status(response_data['homeworks'][0])
-            if last_message != message:
-                send_message(bot, message)
-                timestamp = response_data.get('current_date', timestamp)
-                last_message = message
-        except Exception as error:
-            message = ERROR_MESSAGE.format(error=error)
-            logger.error(message, exc_info=True)
-            send_message(bot, message)
-        finally:
-            time.sleep(RETRY_PERIOD)
-
-
-if __name__ == '__main__':
     main()
